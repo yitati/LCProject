056c1bb0f98155463bd43d48bd037babbc87fc46
Joanne Yang <JoanneY1@verifone.com>
2016-07-30 12:11:56 -0700

    add majority element

--- LCProject/MajorityElement.cpp
+++ LCProject/MajorityElement.cpp
@@ -35,17 +35,34 @@ public:
 
 	}
 
-	int majorityElementII(vector<int>& nums) {
-		int count = 1;
-		int champion = nums[0];
-		for (int i = 1; i < nums.size(); i++) {
-			if (nums[i] == champion) { count++; continue; }
-			else {
-				count--;
-				if (count == 0) { champion = nums[i]; count++; }
+	// there could be at most 2 majority elements in array
+	vector<int> majorityElementII(vector<int>& nums) {
+		vector<int> champions;
+		if (nums.size() == 0) return champions;
+		int champion1 = nums[0], champion2 = nums[0];
+		int count1 = 0, count2 = 0;
+		for (int i = 0; i<nums.size(); i++)
+		{
+			if (nums[i] != champion1 && nums[i] != champion2) {
+				if (count1 == 0) { champion1 = nums[i];  count1 = 1; }
+				else if (count2 == 0) { champion2 = nums[i]; count2 = 1; }
+				else { count1--; count2--; }
 			}
+			else if (nums[i] == champion1) { count1++; continue; }
+			else if (nums[i] == champion2) { count2++; continue; }
 		}
-		return champion;
+		count1 = 0;
+		count2 = 0;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (nums[i] == champion1) count1++;
+			else if (nums[i] == champion2) count2++;
+		}
+
+		if (count1 > nums.size() / 3) champions.push_back(champion1);
+		if (count2 > nums.size() / 3) champions.push_back(champion2);
+		return champions;
 
 	}
 };
@@ -54,9 +71,14 @@ int main(int argc, char * argv[])
 {
 	cout << "Main starts" << endl;
 	MajorityElement major;
-	vector<int> num = { 1, 1, 3, 4, 5, 1, 1 };
+	vector<int> num = { 1, 1, 3, 4, 5, 1, 1, 3, 3, 3, 1 };
 
-	cout << major.majorityElement(num) << endl;
+	vector<int> champions = major.majorityElementII(num);
+	for (int i = 0; i < champions.size(); i++) 
+	{
+		cout << champions[i] << endl;
+	}
+	
 
 	system("pause");
 
