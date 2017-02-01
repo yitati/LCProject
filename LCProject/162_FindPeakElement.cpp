bc7a5931eba6eee01d12a303663289679873454c
VERIFONE\JoanneY1 <joanne.yang@verifone.com>
2017-01-31 23:10:02 -0800

    add find peak element

---
+++ LCProject/162_FindPeakElement.cpp
@@ -0,0 +1,28 @@
+﻿/******************************************************************************/
+/*
+* Question: #162 Find Peak Element
+* A peak element is an element that is greater than its neighbors.
+* Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+* The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+* You may imagine that num[-1] = num[n] = -∞.
+
+For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+*/
+/*****************************************************************************/
+
+#include <vector>
+
+using namespace std;
+
+int findPeakElement(vector<int>& nums)
+{
+	int lhs = 0, rhs = nums.size() - 1;
+	while (lhs + 1 < rhs)
+	{
+		int mid = lhs + (rhs - lhs) / 2;
+		if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
+		else if (nums[mid] < nums[mid - 1]) rhs = mid - 1;
+		else lhs = mid + 1;
+	}
+	return lhs > rhs ? lhs : rhs;
+}
--- LCProject/189_RotateArray.cpp
+++ LCProject/189_RotateArray.cpp
@@ -16,5 +16,9 @@ using namespace std;
 
 void rotate(vector<int>& nums, int k)
 {
-
+	int n = nums.size();
+	k = k % n;
+	reverse(nums.begin(), nums.begin() + n - k);
+	reverse(nums.begin() + n - k, nums.end());
+	reverse(nums.begin(), nums.end());
 }
