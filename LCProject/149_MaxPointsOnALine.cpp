4b41e58c9c6e6253a030e0b815124f6c2669c1ba
yitati <yuanhanlibra@gmail.com>
2017-02-25 20:02:54 -0800

    add single number / max points on a line / excel sheet column / valid number / square root / add binary

---
+++ LCProject/149_MaxPointsOnALine.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+ * Question: #69 Sqrt(x)
+ * Given n points on a 2D plane, find the maximum number of points that lie
+ * on the same straight line.
+ */
+/*****************************************************************************/
+
+#include <vector>
+
+using namespace std;
+
+struct Point{
+  int x;
+  int y;
+  Point(): x(0), y(0){}
+  Point(int a, int b):x(a), y(b){}
+};
+
+int maxPoints(vector<Point>& points)
+{
+  
+
+}
--- LCProject/65_ValidNumber.cpp
+++ LCProject/65_ValidNumber.cpp
@@ -15,9 +15,73 @@
 #include <string>
 
 using namespace std;
-
-
-bool isNumber(string s)
-{
-    // this is only use to test
+bool isNumber(string s) {
+    enum state {SPACE, SIGN, NUM, EXP, EXP_NUM, EXP_SIGN, DOT, NUM_SPACE, NUM_DOT, DOT_NUM};
+    int len = s.length(), i = 0;
+    int state = SPACE;
+    while(i < len)
+    {
+        char curr = s[i++];
+        switch(state)
+        {
+            case SPACE:
+                if(curr == ' ') state = SPACE;
+                else if(curr == '+' || curr == '-') state = SIGN;
+                else if(curr <= '9' && curr >= '0') state = NUM;
+                else if(curr == '.') state = DOT;
+                else return false;
+                break;
+            case SIGN:
+                if(curr <= '9' && curr >= '0') state = NUM;
+                else if(curr == '.') state = DOT;
+                else return false;
+                break;
+            case NUM:
+                if(curr == '.') state = NUM_DOT;
+                else if(curr <= '9' && curr >= '0') state = NUM;
+                else if(curr == 'e') state = EXP;
+                else if(curr == ' ') state = NUM_SPACE;
+                else return false;
+                break;
+            case EXP:
+                if(curr == '+' || curr == '-') state = EXP_SIGN;
+                else if(curr <= '9' && curr >= '0') state = EXP_NUM;
+                else return false;
+                break;
+            case NUM_DOT:
+                if(curr <= '9' && curr >= '0') state = DOT_NUM;
+                else if(curr == ' ') state = NUM_SPACE;
+                else if(curr == 'e') state = EXP;
+                else return false;
+                break;
+            case DOT:
+                if(curr <= '9' && curr >= '0') state = DOT_NUM;
+                else return false;
+                break;
+            case DOT_NUM:
+                if(curr <= '9' && curr >= '0') state = DOT_NUM;
+                else if(curr == ' ') state = NUM_SPACE;
+                else if(curr == 'e') state = EXP;
+                else return false;
+                break;
+            case EXP_NUM:
+                if(curr <= '9' && curr >= '0') state = EXP_NUM;
+                else if(curr == ' ') state = NUM_SPACE;
+                else return false;
+                break;
+            case EXP_SIGN:
+                if(curr <= '9' && curr >= '0') state = EXP_NUM;
+                else return false;
+                break;
+            case NUM_SPACE:
+                if(curr == ' ') state = NUM_SPACE;
+                else return false;
+                break;
+            default:
+                return false;
+                break;
+        }
+    }
+    if(state == NUM || state == EXP_NUM || state == DOT_NUM || state == NUM_DOT || state == NUM_SPACE) return true;
+    return false;
 }
